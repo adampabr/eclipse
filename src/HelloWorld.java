0edd0e18bb7d4563e04f39cff5336bc07b73ec40
adamp <adamp@Adam.lan>
2024-09-16 13:04:25 +0930

    Commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,12 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		HelloWorld hw = new HelloWorld();
+		hw.printHello();
+	}
+
+	public void printHello() {
+		System.out.println("Hello, World!");
+	}
+}
